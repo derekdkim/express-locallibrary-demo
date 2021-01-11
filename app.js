2e66ceba79195fda4f616eb337ac394d1d8ffcfd
Derek Kim <ddkim1024@gmail.com>
2021-01-11 17:32:46 -0600

    Added files and changes required for heroku deployment

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(compression());
 app.use(helmet());
 
 // Set up mongoose connection
-var mongoDB = 'mongodb+srv://admin:admin@cluster0.m8qnv.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://admin:admin@cluster0.m8qnv.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
